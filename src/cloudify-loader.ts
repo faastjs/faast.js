a398380fc812ecd89139fa6afea3bfc3cb4eac02
Andy Chou <acchou2@gmail.com>
2018-05-17 22:48:45 -0700

    remove verbose logging

--- src/cloudify-loader.ts
+++ src/cloudify-loader.ts
@@ -3,15 +3,11 @@ import { getOptions } from "loader-utils";
 import * as path from "path";
 import { log } from "./log";
 
-export default function webpackCloudifyLoader(this: any, source: string) {
+export default function webpackCloudifyLoader(this: any, _source: string) {
     const options = getOptions(this);
-    log(humanStringify(options));
-    log(`RUNNING LOADER, source: ${source}, entry: ${options.entry}`);
     const fspath = path.join(__dirname, "functionserver.js");
-    const rv = `
+    return `
         exports.trampoline = require("${fspath}").trampoline;
         require("${options.entry}");
     `;
-    log(rv);
-    return rv;
 }
