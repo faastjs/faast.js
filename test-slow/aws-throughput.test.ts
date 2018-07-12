9d14864738b1c1cdd2cb5cb25e1399038cab2e53
Andy Chou <acchou2@gmail.com>
2018-07-12 14:40:00 -0700

    reduce throughput test to 60s. Cost is about $0.50

--- test-slow/aws-throughput.test.ts
+++ test-slow/aws-throughput.test.ts
@@ -1,6 +1,6 @@
 import { throughputTest } from "./tests";
 
-throughputTest("AWS throughput test", "aws", 600 * 1000, {
+throughputTest("AWS throughput test", "aws", 60 * 1000, {
     memorySize: 1024,
     useQueue: true
 });
--- test-slow/google-throughput.test.ts
+++ test-slow/google-throughput.test.ts
@@ -1,6 +1,6 @@
 import { throughputTest } from "./tests";
 
-throughputTest("Google throughput test", "google", 600 * 1000, {
+throughputTest("Google throughput test", "google", 60 * 1000, {
     memorySize: 1024,
     useQueue: false
 });
