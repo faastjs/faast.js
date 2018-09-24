4671a068a8779b425f64bd36ec8d372c00333c68
Andy Chou <acchou2@gmail.com>
2018-09-24 13:21:49 -0700

    remove extraneous error log message

--- src/google/google-cloudify.ts
+++ src/google/google-cloudify.ts
@@ -498,11 +498,6 @@ async function callFunction(state: State, callRequest: FunctionCall) {
                 return callFunctionHttps(state.url!, callRequest, state.metrics, n).catch(
                     rawResponse => {
                         const { response } = rawResponse;
-                        console.log(
-                            `Error: ${response.status} ${response.statusText} ${
-                                response.data
-                            }`
-                        );
                         if (response.status === 503 || response.status === 408) {
                             return createErrorReturn(rawResponse, n);
                         }
