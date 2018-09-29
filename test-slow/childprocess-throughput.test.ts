103294146b94ac29776fcca3b6a4c404ba9cb30f
Andy Chou <acchou2@gmail.com>
2018-09-28 23:13:08 -0700

    update

--- test-slow/childprocess-throughput.test.ts
+++ test-slow/childprocess-throughput.test.ts
@@ -1,5 +1,5 @@
 import { throughputTest } from "./tests";
 
-throughputTest("Child process throughput test", "childprocess", 60 * 1000, 200, {
+throughputTest("Child process throughput test", "childprocess", 60 * 1000, 95, {
     memorySize: 64
 });
