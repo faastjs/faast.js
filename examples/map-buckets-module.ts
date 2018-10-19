7f89bc0d0d5c6b7659c0e3045ed470bdf93146f8
Andy Chou <acchou2@gmail.com>
2018-10-18 17:36:25 -0700

    Add basic map over s3 buckets example, which currently just untars .tar files and uploads the resulting files.

--- examples/map-buckets-module.ts
+++ examples/map-buckets-module.ts
@@ -2,17 +2,54 @@ import * as aws from "aws-sdk";
 import * as tar from "tar-stream";
 import * as stream from "stream";
 
-const s3 = new aws.S3();
+const s3 = new aws.S3({ region: "us-west-2" });
 
-export async function processBucketObject(Bucket: string, Key: string) {
-    const result = await s3.getObject({ Bucket, Key }).promise();
-    const extract: tar.Extract = tar.extract();
-    extract.on("entry", (header, tarstream, next) => {
-        console.log(`Entry: ${header.name}, size: ${header.size}`);
-        tarstream.on("end", next);
+export async function extractTarBuffer(
+    content: Buffer,
+    processEntry: (header: tar.Headers, tarstream: stream.Readable) => Promise<void>
+) {
+    const tarExtract: tar.Extract = tar.extract();
+    tarExtract.on("entry", async (header, tarstream, next) => {
+        await processEntry(header, tarstream);
+        next();
     });
     const readable = new stream.Readable();
-    readable.push(result.Body!);
+    readable._read = () => {};
+    readable.push(content);
     readable.push(null);
-    readable.pipe(extract);
+    readable.pipe(tarExtract);
+    await new Promise(resolve => readable.on("end", resolve));
+}
+
+export async function processBucketObject(Bucket: string, Key: string) {
+    const start = Date.now();
+    console.log(`ProcessBucketObject called: Bucket: ${Bucket}, Key: ${Key}`);
+
+    console.log(`${Date.now() - start} Starting download`);
+    const result = await s3.getObject({ Bucket, Key }).promise();
+    console.log(`${Date.now() - start} Download finished`);
+    console.log(`Result: %O`, result);
+    if (result.ContentType === "application/x-directory") {
+        console.log(`Directory entry. Skipping.`);
+        return;
+    }
+    extractTarBuffer(result.Body! as Buffer, async (header, tarstream) => {
+        if (header.type === "file") {
+            console.log(
+                `${Date.now() - start}s Uploading ${header.name}, size: ${header.size}`
+            );
+            s3.putObject({
+                Bucket: "arxiv-derivative",
+                Key: `extracted/${header.name}`,
+                Body: tarstream,
+                ContentLength: header.size
+            })
+                .promise()
+                .then(_ => {
+                    console.log(
+                        `${(Date.now() - start) / 1000}s Uploaded ${header.name}`
+                    );
+                });
+        }
+    });
 }
--- examples/map-buckets.ts
+++ examples/map-buckets.ts
@@ -1,15 +1,23 @@
 import { cloudify } from "../src/cloudify";
 import * as m from "./map-buckets-module";
 import * as aws from "aws-sdk";
+import * as fs from "fs";
 
 const s3 = new aws.S3();
 
 export async function mapBucket(Bucket: string) {
-    const { cloudFunc, remote } = await cloudify("aws", m, "./module");
+    const { cloudFunc, remote } = await cloudify("aws", m, "./map-buckets-module", {
+        memorySize: 2048,
+        timeout: 300
+    });
     try {
         const objects = await s3.listObjectsV2({ Bucket }).promise();
         console.log(`Bucket ${Bucket} contains ${objects.Contents!.length} objects`);
-        await remote.processBucketObject(Bucket, objects.Contents![0].Key!);
+        for (const Obj of objects.Contents!) {
+            if (Obj.Key!.match(/^pdf\//)) {
+                await remote.processBucketObject(Bucket, Obj.Key!);
+            }
+        }
     } finally {
         const cost = await cloudFunc.costEstimate();
         console.log(`${cost}`);
@@ -17,4 +25,24 @@ export async function mapBucket(Bucket: string) {
     }
 }
 
-mapBucket("arxiv-derivative");
+mapBucket("arxiv-derivative-west");
+
+function localMain() {
+    const file = process.argv[2];
+    console.log(`Processing tar file ${file}`);
+    m.extractTarBuffer(fs.readFileSync(file), async (header, tarstream) => {
+        if (header.type === "file") {
+            const start = Date.now();
+            console.log(`Uploading ${header.name}`);
+            await s3
+                .putObject({
+                    Bucket: "arxiv-derivative",
+                    Key: `extracted/${header.name}`,
+                    Body: tarstream,
+                    ContentLength: header.size
+                })
+                .promise();
+            console.log(`${(Date.now() - start) / 1000}s Uploaded ${header.name}`);
+        }
+    });
+}
