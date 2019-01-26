3ebf1f267fd75e455ab39ebd7a55fb907eaf7fb8
Andy Chou <acchou2@gmail.com>
2019-01-26 10:23:51 -0800

    longer timeout for another set of google tests.

--- test/google-https-childprocess.test.ts
+++ test/google-https-childprocess.test.ts
@@ -1,4 +1,5 @@
 import { testFunctions } from "./tests";
 
+jest.setTimeout(10 * 1000);
 describe("google-https with child process", () =>
     testFunctions("google", { mode: "https", childProcess: true }));
--- test/google-queue-childprocess.test.ts
+++ test/google-queue-childprocess.test.ts
@@ -1,4 +1,5 @@
 import { testFunctions } from "./tests";
 
+jest.setTimeout(10 * 1000);
 describe("google-queue with child process", () =>
     testFunctions("google", { mode: "queue", childProcess: true }, 360 * 1000));
