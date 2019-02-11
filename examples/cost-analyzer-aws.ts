af9954631e51c32904a560e95abad53ae0208ad4
Andy Chou <acchou2@gmail.com>
2019-02-11 12:42:22 -0800

    Write out csv output for aws cost analyzer example.

--- examples/cost-analyzer-aws.ts
+++ examples/cost-analyzer-aws.ts
@@ -1,16 +1,20 @@
 import { costAnalyzer, Promisified } from "../src/faast";
 import * as m from "./module";
+import { toCSV } from "../src/cost";
+import { writeFile } from "../src/fs";
 
 async function work(remote: Promisified<typeof m>) {
-    await remote.randomNumbers(20000000);
+    await remote.randomNumbers(100000000);
 }
 
 async function compareAws() {
-    costAnalyzer.estimateWorkloadCost(
+    const results = await costAnalyzer.estimateWorkloadCost(
         require.resolve("./module"),
         costAnalyzer.awsConfigurations,
         { work }
     );
+
+    await writeFile("cost.csv", toCSV(results));
 }
 
 compareAws();
