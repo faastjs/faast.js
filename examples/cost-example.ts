bfe8f173ef0b8762b7fdb54982b30ca97531a100
Andy Chou <acchou2@gmail.com>
2019-03-28 09:06:33 -0700

    Fix example

--- examples/cost-example.ts
+++ examples/cost-example.ts
@@ -2,7 +2,7 @@ import { faast } from "../index";
 import * as m from "./functions";
 
 async function main() {
-    const faastModule = await faast("aws", m, "./module");
+    const faastModule = await faast("aws", m, "./functions");
 
     const result = await faastModule.functions.hello("world");
     const cost = await faastModule.costSnapshot();
