28d213c4338a3dace5c4642108a9915331e86f6e
Andy Chou <acchou2@gmail.com>
2019-03-29 18:02:57 -0700

    Remove some unneeded files. No longer using AWS or Google builds. No longer using Jest.

--- types/jest.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace jest {
-    interface Each {
-        <T extends any[]>(cases: T[]): (name: string, fn: (...args: T) => any) => void;
-    }
-    interface Describe {
-        each: Each;
-    }
-}
