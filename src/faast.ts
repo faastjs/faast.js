6f3d29ff8190cb94de1267d88e4bbe3c49a290c5
teppeis <teppeis@gmail.com>
2019-05-16 08:51:33 +0900

    fix: overload faast() for specific providers

--- src/faast.ts
+++ src/faast.ts
@@ -928,6 +928,26 @@ function resolve(fmodule: object) {
  * @public
  */
 export async function faast<M extends object>(
+    provider: "aws",
+    fmodule: M,
+    options?: AwsOptions
+): Promise<FaastModule<M>>;
+export async function faast<M extends object>(
+    provider: "google",
+    fmodule: M,
+    options?: GoogleOptions
+): Promise<FaastModule<M>>;
+export async function faast<M extends object>(
+    provider: "local",
+    fmodule: M,
+    options?: LocalOptions
+): Promise<FaastModule<M>>;
+export async function faast<M extends object>(
+    provider: Provider,
+    fmodule: M,
+    options?: CommonOptions
+): Promise<FaastModule<M>>;
+export async function faast<M extends object>(
     provider: Provider,
     fmodule: M,
     options?: CommonOptions
